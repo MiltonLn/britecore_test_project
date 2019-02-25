30821d12e46378c39fcf4b966c4668570d727db1
MiltonLn <miltonln04@gmail.com>
2019-02-24 21:49:46 -0500

    Adding DNS to allowed hosts and adding gunicorn script

---
+++ backend/bin/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NAME="backend"
+REPO_DIR=/home/ubuntu/britecore_test_project
+DJANGO_DIR="$REPO_DIR/backend"
+SOCKET_FILE=/home/ubuntu/run/gunicorn.sock
+USER="ubuntu"
+NUM_WORKERS=3
+DJANGO_SETTINGS_MODULE=config.settings
+DJANGO_WSGI_MODULE=config.wsgi
+
+echo "Starting $NAME as `whoami`"
+
+cd $DJANGO_DIR
+source venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGO_DIR:$PYTHONPATH
+
+RUN_DIR=$(dirname $SOCKET_FILE)
+test -d $RUN_DIR || mkdir -p $RUN_DIR
+
+gunicorn ${DJANGO_WSGI_MODULE}\
+	--name $NAME\
+	--workers $NUM_WORKERS\
+	--user=$USER\
+	--bind=unix:$SOCKET_FILE\
+	--log-level=debug\
+	--log-file=-
+
